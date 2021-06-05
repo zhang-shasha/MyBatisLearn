1133e7000fea307adc5b081107cac0d4a72a7f7c
zhangmingshi <zhangmingshi75@gmail.com>
2021-06-05 17:35:30 +0800

    Map使用和模糊查询

--- mybatis-01/src/main/java/com/hbuas/Dao/UserMapper.java
+++ mybatis-01/src/main/java/com/hbuas/Dao/UserMapper.java
@@ -3,6 +3,7 @@ package com.hbuas.Dao;
 import com.hbuas.POJO.User;
 
 import java.util.List;
+import java.util.Map;
 
 public interface UserMapper {
     //获取全部用户
@@ -15,4 +16,8 @@ public interface UserMapper {
     int updateUser(User user);
     //删除一个用户
     int deleteUser(int id);
+    //万能的Map的应用
+    int addUser2(Map<String,Object> map);
+    //模糊查询
+    List<User> getUserLikes(String name);
 }
--- mybatis-01/src/test/java/com/hbuas/Dao/UserDaoTest.java
+++ mybatis-01/src/test/java/com/hbuas/Dao/UserDaoTest.java
@@ -5,7 +5,9 @@ import com.hbuas.Utils.MybatisUtils;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class UserDaoTest {
     @Test
@@ -55,6 +57,26 @@ public class UserDaoTest {
         UserMapper mapper = sqlSession.getMapper(UserMapper.class);
         mapper.deleteUser(4);
         sqlSession.commit();
+        sqlSession.close();
+    }
+    @Test
+    public void addUser2(){
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        Map<String,Object> map = new HashMap<String,Object>();
+        map.put("userId",5);
+        map.put("userName","WuZhao");
+        map.put("password","445566");
+        mapper.addUser2(map);
+        sqlSession.commit();
+        sqlSession.close();
+    }
+    @Test
+    public void getUserLikes(){
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        List<User> userList = mapper.getUserLikes("zhan");
+        System.out.println(userList);
         sqlSession.close();
     }
 
