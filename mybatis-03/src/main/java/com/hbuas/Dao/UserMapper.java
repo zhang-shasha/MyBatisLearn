a975978cf92d3bef252b8f07def7dbd341b0262e
zhangmingshi <zhangmingshi75@gmail.com>
2021-06-17 10:48:23 +0800

    分页、分页插件

--- mybatis-03/src/main/java/com/hbuas/Dao/UserMapper.java
+++ mybatis-03/src/main/java/com/hbuas/Dao/UserMapper.java
@@ -3,10 +3,13 @@ package com.hbuas.Dao;
 import com.hbuas.POJO.User;
 
 import java.util.List;
+import java.util.Map;
 
 public interface UserMapper {
     //获取全部用户
     List<User> getUserList();
     //根据id查询用户
     User getUserById(int id);
+    //分页查询
+    List<User> getUserByLimit(Map<String,Integer> map);
 }
--- mybatis-03/src/test/java/Dao/UserDaoTest.java
+++ mybatis-03/src/test/java/Dao/UserDaoTest.java
@@ -1,12 +1,15 @@
 package Dao;
 
+import com.github.pagehelper.PageHelper;
 import com.hbuas.Dao.UserMapper;
 import com.hbuas.POJO.User;
 import com.hbuas.Utils.MybatisUtils;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class UserDaoTest {
     @Test
@@ -15,6 +18,8 @@ public class UserDaoTest {
         SqlSession sqlSession = MybatisUtils.getSqlSession();
         //执行SQL
         UserMapper userDao = sqlSession.getMapper(UserMapper.class);
+        //调用分页插件
+        PageHelper.startPage(1,3);
         List<User> userList = userDao.getUserList();
         for(User user : userList){
             System.out.println(user);
@@ -28,6 +33,19 @@ public class UserDaoTest {
         UserMapper mapper = sqlSession.getMapper(UserMapper.class);
         User user = mapper.getUserById(1);
         System.out.println(user);
+        sqlSession.close();
+    }
+    @Test
+    public void getUserByLimit(){
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        Map<String,Integer> map = new HashMap<>();
+        map.put("startIndex",1);
+        map.put("pageSize",2);
+        List<User> userList = mapper.getUserByLimit(map);
+        for(User user : userList){
+            System.out.println(user);
+        }
         sqlSession.close();
     }
 
