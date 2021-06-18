aec8824fa5ac0957feacfacecc600a72f241cd7b
zhangmingshi <zhangmingshi75@gmail.com>
2021-06-18 16:13:38 +0800

    set 标签的使用

--- mybatis-08/src/main/java/com/hbuas/Dao/BlogMapper.java
+++ mybatis-08/src/main/java/com/hbuas/Dao/BlogMapper.java
@@ -9,4 +9,8 @@ public interface BlogMapper {
     int addBlog(Blog blog);
     //查询所有的Blogs，使用动态SQL形式
     List<Blog> getBlogs(Map map);
+    //使用choose标签查询
+    List<Blog> getBlogsChoose(Map map);
+    //更新博客使用 SET标签
+    int updateBlog(Map map);
 }
--- mybatis-08/src/test/java/com/hbuas/Dao/BlogMapperTest.java
+++ mybatis-08/src/test/java/com/hbuas/Dao/BlogMapperTest.java
@@ -9,7 +9,6 @@ import org.junit.Test;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class BlogMapperTest {
     @Test
@@ -29,12 +28,24 @@ public class BlogMapperTest {
     public void getBlogs(){
         SqlSession sqlSession = MybatisUtils.getSqlSession();
         BlogMapper mapper = sqlSession.getMapper(BlogMapper.class);
-        Map map = new HashMap<>();
+        HashMap<Object, Object> map = new HashMap<>();
 //        map.put("author","zhang");
         map.put("title","LearnVUE");
-        List<Blog> blogList = mapper.getBlogs(map);
+        List<Blog> blogList = mapper.getBlogsChoose(map);
         for (Blog blog : blogList) {
             System.out.println(blog);
         }
+        sqlSession.close();
+    }
+    @Test
+    public void updateBlog(){
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        BlogMapper mapper = sqlSession.getMapper(BlogMapper.class);
+        HashMap<Object, Object> map = new HashMap<>();
+//        map.put("title","LearnSpring2");
+        map.put("author","Sun");
+        map.put("id","946640722e4f40b8afb06d1f200ee4b0");
+        mapper.updateBlog(map);
+        sqlSession.close();
     }
 }
